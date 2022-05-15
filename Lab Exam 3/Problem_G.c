5bfc8f5ebfb8fd9db70a632f5b1e313e6664c49a
RomanSheikh99 <romansheikh170@gmail.com>
2022-05-16 01:18:58 +0600

    problem_G

--- Lab Exam 3/Problem_G.c
+++ Lab Exam 3/Problem_G.c
@@ -6,26 +6,6 @@ int main(){
     scanf("%d", &t);
     for(int i = 0; i < t; i++){
 
-//        char str[101];
-//        scanf("%s", str);
-//
-//        int letter[125]= {0};
-//
-//        for(int i = 0; i < strlen(str); i++){
-//            int temp = str[i];
-//            printf("%d ", temp);
-//            letter[temp]++;
-//        }
-//
-////        int pop = 0;
-//        for(int i = 97; i <= 122; i++){
-//            if(letter[i] > 0){
-//                char L = i;
-//                printf("%c ", L);
-//            }
-//        }
-
-
         char str[100];
         scanf("%s", str);
 
@@ -48,3 +28,44 @@ int main(){
     return 0;
 }
 
+
+
+// way 2 start 
+
+
+/*
+
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i = 0; i < t; i++){
+
+       char str[101];
+       scanf("%s", str);
+
+       int letter[125]= {0};
+
+       for(int i = 0; i < strlen(str); i++){
+           int temp = str[i];
+           printf("%d ", temp);
+           letter[temp]++;
+       }
+
+       for(int i = 97; i <= 122; i++){
+           if(letter[i] > 0){
+               char L = i;
+               printf("%c ", L);
+           }
+       }
+
+
+
+        printf("\n");
+    }
+
+    return 0;
+}
+*\
